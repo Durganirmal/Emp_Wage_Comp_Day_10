a76ed62b3290e040dd31b9f023e6a98395840f37
Durga Nirmal <durganirmal123@gmail.com>
2021-11-16 15:51:44 +0530

    Added : Welcome Message

---
+++ Employee_Wage_Computation.java
@@ -0,0 +1,10 @@
+package com.Emp_Wage_Comp;
+
+public class Employee_Wage_Computation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage computation Program!!");
+
+	}
+
+}
